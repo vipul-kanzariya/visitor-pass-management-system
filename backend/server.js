3f4e5feb2304b8e9148d7c9c2d0108bf70dde5ce
vipul-kanzariya <vipulkanzariya026@gmail.com>
2026-07-03 19:17:54 +0530

    update

--- backend/server.js
+++ backend/server.js
@@ -16,7 +16,7 @@ dotenv.config();
 
 app.use(express.json());
 app.use(cors({
-    origin: 'https://your-netlify-url.netlify.app'  
+    origin: 'https://visitor-pass-management-system-1cve.onrender.com'
 }));
 app.use((req, res, next) => {
   console.log(req.path, req.method);
