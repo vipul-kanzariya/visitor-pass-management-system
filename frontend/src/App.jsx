6eec27f7db8202b7b559035c35679cdb4582d79b
vipul-kanzariya <vipulkanzariya026@gmail.com>
2026-07-03 18:56:00 +0530

    add default root

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,29 +1,55 @@
-import {BrowserRouter,Routes,Route} from 'react-router-dom';
-import Login from './pages/Login';
-import { ProtectedRoute } from './components/ProtectedRoute';
-import Dashboard from './pages/Dashboard';
-import Visitors from './pages/Visitors';
-import Pass from './pages/Pass';
-import NavBar from './components/NavBar';
-import RegisterUser from './pages/RegisterUser';
-import { useAuth } from './context/AuthContext'
-function App() {
-     const { token } = useAuth();
-    return (
-        
-    <BrowserRouter>
-            {token && <NavBar />}
-        <Routes>
-            <Route path='/login' element={<Login/>}/>
-            <Route path='/register' element={<RegisterUser/>}/>
-            <Route path='/dashboard' element={<ProtectedRoute><Dashboard/></ProtectedRoute>}/>
-            <Route path='/visitor' element={<ProtectedRoute><Visitors/></ProtectedRoute>}/>
-            <Route path='/pass' element={<ProtectedRoute><Pass/></ProtectedRoute>}/>
-            <Route path='/' element={<h1 className='bg-gray-500 p-4 m-4 text-info'>Home Page</h1>}/>
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import Login from "./pages/Login";
+import RegisterUser from "./pages/RegisterUser";
+import Dashboard from "./pages/Dashboard";
+import Visitors from "./pages/Visitors";
+import Pass from "./pages/Pass";
+import NavBar from "./components/NavBar";
+import { ProtectedRoute } from "./components/ProtectedRoute";
+import { useAuth } from "./context/AuthContext";
 
-        </Routes>
+function App() {
+  const { token } = useAuth();
+
+  return (
+    <BrowserRouter>
+      {token && <NavBar />}
+
+      <Routes>
+        <Route path="/" element={<Navigate to="/register" replace />} />
+
+        <Route path="/register" element={<RegisterUser />} />
+        <Route path="/login" element={<Login />} />
+
+        <Route
+          path="/dashboard"
+          element={
+            <ProtectedRoute>
+              <Dashboard />
+            </ProtectedRoute>
+          }
+        />
+
+        <Route
+          path="/visitor"
+          element={
+            <ProtectedRoute>
+              <Visitors />
+            </ProtectedRoute>
+          }
+        />
+
+        <Route
+          path="/pass"
+          element={
+            <ProtectedRoute>
+              <Pass />
+            </ProtectedRoute>
+          }
+        />
+      </Routes>
     </BrowserRouter>
-);
+  );
 }
 
 export default App;
